2dfd5f5649761988ac276844ae59b256938ea348
prqr <prqrprqr@gmail.com>
2021-08-05 08:33:36 +0000

    kadai4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+   resource :relationships, only: [:create, :destroy]
+   member do
+      get 'followers'
+      get 'followings'
+    end
+
+  	end
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  
+  
+  	
+  	
 end
